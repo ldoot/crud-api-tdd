c417fc6f6f78e3959950c813cfd53ce75aca30b7
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-02 11:00:12 +0100

    Mock req and res for createTodo test

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,5 +1,7 @@
 const todoController = require("../../controllers/todo.controller");
 const todoModel = require("../../models/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 //mocking the create Function using jest so that we will be able to see if the function gets called during one of our tests.
 todoModel.create = jest.fn();
@@ -10,7 +12,17 @@ describe("TodoController.createTodo", () => {
   });
 
   it("should call todoModel.create", () => {
-    todoController.createTodo();
-    expect(todoModel.create).toBeCalled();
+    let req, res, next;
+
+    // Configure http request and response objects as mocks
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+
+    // Configure request body as a mock newTodo
+    req.body = newTodo;
+
+    todoController.createTodo(req, res, next);
+    expect(todoModel.create).toBeCalledWith(newTodo);
   });
 });
