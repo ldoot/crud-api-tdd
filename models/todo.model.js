aa7523c0f8aa018c5c651ccdd83e93402be10868
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-02 10:30:28 +0100

    Implement basic todo mongoose model

---
+++ models/todo.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  done: { type: Boolean, required: true },
+});
+
+const todoModel = mongoose.model("Todo", todoSchema);
+
+module.exports = todoModel;
