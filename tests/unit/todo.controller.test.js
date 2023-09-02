0179a2a81916bb8d88169313ab9798789302b4af
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-02 11:27:19 +0100

    Create todo controller setup logic

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -6,6 +6,17 @@ const newTodo = require("../mock-data/new-todo.json");
 //mocking the create Function using jest so that we will be able to see if the function gets called during one of our tests.
 todoModel.create = jest.fn();
 
+// Http objects for mocking during testing.
+let req, res, next;
+
+//Setup for each test
+beforeEach(() => {
+  // Configure http request and response objects as mocks
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof todoController.createTodo).toBe("function");
