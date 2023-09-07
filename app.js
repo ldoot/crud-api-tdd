a7ebe180ab900b46156d3268d95d2306ea0512fe
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-07 13:33:37 +0100

    Add todo routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const todoRoutes = require("./routes/todo.routes");
 // Setup express app
 const app = express();
+
+app.use("/todos", todoRoutes);
 
 app.get("/", (request, response) => {
   response.json("Hello world!");
@@ -10,3 +12,5 @@ app.get("/", (request, response) => {
 app.listen(3000, () => {
   console.log("Server listening for requests.");
 });
+
+module.exports = app;
