44c123d4a67d491b14166fe89b102c1aabcfb161
Lucas Holmes <lucas.holmes@backslash.build>
2023-09-08 13:38:18 +0100

    Add getTodos unit tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -2,9 +2,11 @@ const todoController = require("../../controllers/todo.controller");
 const todoModel = require("../../models/todo.model");
 const httpMocks = require("node-mocks-http");
 const newTodo = require("../mock-data/new-todo.json");
+const allTodos = require("../mock-data/all-todos.json");
 
 //mocking the create Function using jest so that we will be able to see if the function gets called during one of our tests.
 todoModel.create = jest.fn();
+todoModel.find = jest.fn();
 
 // Http objects for mocking during testing.
 let req, res, next;
@@ -15,6 +17,36 @@ beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
   next = jest.fn();
+});
+
+describe("todoController.getTodos", () => {
+  it("should have a createTodo function", () => {
+    expect(typeof todoController.getTodos).toBe("function");
+  });
+
+  it("should call todoModel.find({})", async () => {
+    await todoController.getTodos(req, res, next);
+    expect(todoModel.find).toBeCalledWith({});
+  });
+
+  it("should return status code 200 and all todos", async () => {
+    todoModel.find.mockReturnValue(allTodos);
+
+    await todoController.getTodos(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+    expect(res._getJSONData()).toStrictEqual(allTodos);
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Simulated error" };
+    const rejectedPromise = Promise.reject(errorMessage);
+
+    todoModel.find.mockReturnValue(rejectedPromise);
+    await todoController.getTodos(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
+  });
 });
 
 describe("TodoController.createTodo", () => {
